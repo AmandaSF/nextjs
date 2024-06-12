d2ff1aee4f1dca9cc14f21fc8bef367e97454870
AmandaSF <amandafuhrmann@gmail.com>
2024-06-11 17:27:10 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -16,6 +16,7 @@ export default function Home() {
         <p>2)Run ticket review sessions to make sure tone is consistent: I think finding the balance between consistent tone and allowing agents to use their own personalities in responses allows for work to be a lot more enjoyable. Supporting those efforts is enjoyable to me, and crafting ideal responses or reworking responses is also a great place to create new macros</p>
         <p>{'3)Manage a support team: Having the luxury of dedicated time for managing a team is a dream. Having that chance to dig into goals, where people are spiking, what they are struggling with, and getting to think about how to level people is really rewarding'}</p>
         <p>4)Work with the product team to develop a new feature based on feedback from customers: Cross functional work tends to drive a product forward like nothing else. It can be hard to allocate time for it, but the reward when a desired feature launches is very high</p>
+        <p>5)Help train and onboard new support teammates: This goes hand in hand with 3, but iterating and making sure your training stays relevant is an ongoing process that allows you to see how much the product has matured. When you get to remove a training module because those problems have been solved, or bring in a new one due to new product launches-- it's fun!</p>
       </div>
     </main>
   );
